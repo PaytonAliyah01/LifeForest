9b95fec98912ef15d7b8b7b04f3b7eb1525e9219
Elienne Phelipa <tiffany.phelipa@oulook.com>
2026-04-17 10:13:42 +0200

    fix some build issues

--- backend/src/test/java/com/lifeforest/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/lifeforest/backend/BackendApplicationTests.java
@@ -3,9 +3,7 @@ package com.lifeforest.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
-})
+@SpringBootTest
 class BackendApplicationTests {
 
     @Test
